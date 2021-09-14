3f17c3adac0653653d59c3d8b7f62ee2f9c42678
fatih <48352716+secedit@users.noreply.github.com>
2021-09-15 01:35:38 +0200

    Update webInsurance.py

--- webInsurance.py
+++ webInsurance.py
@@ -1,12 +1,5 @@
 import numpy as np  # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report, confusion_matrix
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
-from imblearn.over_sampling import SMOTE
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix, ConfusionMatrixDisplay
 import streamlit as st
 
 
